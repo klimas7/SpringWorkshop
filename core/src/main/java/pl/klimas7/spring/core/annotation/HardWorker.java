eabb04a46558f8fbde6dd79c0f37249b25523fc6
klimas7 <klimas7@gmail.com>
2020-12-05 12:18:10 +0100

    annotation configuration #2: Factory part 1

--- core/src/main/java/pl/klimas7/spring/core/annotation/HardWorker.java
+++ core/src/main/java/pl/klimas7/spring/core/annotation/HardWorker.java
@@ -1,5 +1,26 @@
 package pl.klimas7.spring.core.annotation;
 
-public class HardWorker {
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Log
+@Component
+public class HardWorker implements Worker{
+    private final String uuid;
+
+    public HardWorker() {
+        uuid = UUID.randomUUID().toString();
+    }
+
+    @Override
+    public void doWork() {
+        log.info("I'm Hard worker nr: " + uuid);
+    }
+
+    private void born() {
+        log.info("Born a hard worker nr: " + uuid);
+    }
 
 }
--- core/src/main/java/pl/klimas7/spring/core/annotation/LazyWorker.java
+++ core/src/main/java/pl/klimas7/spring/core/annotation/LazyWorker.java
@@ -1,4 +1,25 @@
 package pl.klimas7.spring.core.annotation;
 
-public class LazyWorker {
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Log
+@Component
+public class LazyWorker implements Worker{
+    private final String uuid;
+
+    public LazyWorker() {
+        this.uuid = UUID.randomUUID().toString();
+    }
+
+    @Override
+    public void doWork() {
+        log.info("I'm Lazy worker nr: " + uuid);
+    }
+
+    private void born() {
+        log.info("Born a lazy worker nr: " + uuid);
+    }
 }
