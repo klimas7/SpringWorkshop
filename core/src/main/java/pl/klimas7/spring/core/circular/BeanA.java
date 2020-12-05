706eabb30962502a8a08210a4024cd3eacbe7c46
klimas7 <klimas7@gmail.com>
2020-12-05 15:09:35 +0100

    Circular example #5: final solution

--- core/src/main/java/pl/klimas7/spring/core/circular/BeanA.java
+++ core/src/main/java/pl/klimas7/spring/core/circular/BeanA.java
@@ -1,17 +1,26 @@
 package pl.klimas7.spring.core.circular;
 
+import lombok.Getter;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Log
+@Getter
 @Component
 public class BeanA {
-    private static final String MESSAGE = "BeanA";
+    private final String MESSAGE = "BeanA";
     private final BeanB beanB;
 
     public BeanA(BeanB beanB) {
         this.beanB = beanB;
-        this.beanB.printMessage();
+        log.info("In constructor BeanA");
+    }
+
+    public String getOtherMessage() {
+        log.info("BeanA: getOtherMessage");
+        return this.beanB.getMESSAGE();
     }
 
     public void printMessage() {
--- core/src/main/java/pl/klimas7/spring/core/circular/BeanB.java
+++ core/src/main/java/pl/klimas7/spring/core/circular/BeanB.java
@@ -1,19 +1,29 @@
 package pl.klimas7.spring.core.circular;
 
+import lombok.Getter;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Log
 @Component
+@Getter
 public class BeanB {
-    private static final String MESSAGE = "BeanB";
+    private final String MESSAGE = "BeanB";
     private final BeanA beanA;
 
     public BeanB(@Lazy BeanA beanA) {
         this.beanA = beanA;
-        this.beanA.printMessage();
+        log.info("In constructor BeanB");
     }
+
+    public String getOtherMessage() {
+        log.info("BeanB: getOtherMessage");
+        return this.beanA.getMESSAGE();
+    }
+
 
     public void printMessage() {
         log.info(MESSAGE);
