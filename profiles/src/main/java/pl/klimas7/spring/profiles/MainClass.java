23b016eebf4d16ff3946193a0d7a4ec92863a432
klimas7 <klimas7@gmail.com>
2020-12-05 15:40:30 +0100

    profiles #1: prepare template

--- profiles/src/main/java/pl/klimas7/spring/profiles/MainClass.java
+++ profiles/src/main/java/pl/klimas7/spring/profiles/MainClass.java
@@ -1,10 +1,24 @@
 package pl.klimas7.spring.profiles;
 
+import lombok.extern.java.Log;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+@Log
 public class MainClass {
     static {
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-7s] %5$s %n");
     }
 
     public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext("pl.klimas7.spring.profiles");
+
+        Arrays.asList(ctx.getBeanDefinitionNames()).forEach(log::info);
+
+        log.info("----------------");
+        var message = ctx.getBean("message", Message.class);
+        message.sendMessage();
     }
 }
--- profiles/src/main/java/pl/klimas7/spring/profiles/SpringConfig.java
+++ profiles/src/main/java/pl/klimas7/spring/profiles/SpringConfig.java
@@ -1,6 +1,23 @@
 package pl.klimas7.spring.profiles;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfig {
+    @Bean
+    public Message messageTest() {
+        return new MessageImpl("SimpleBeanInTest");
+    }
+
+    @Bean
+    public Message message() {
+        return new MessageImpl("SimpleBean");
+    }
+
+
+
+
 
     /*
      * Definiowanie dwóch Beanów o tej samej name w tym wypadku jest ok jednak będzie brany pod uwagę pierwszy
