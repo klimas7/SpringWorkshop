8d7615ee68b16b7a526f8cb5e85004a9af705169
klimas7 <klimas7@gmail.com>
2020-12-05 15:31:11 +0100

    Depends on #1

--- core/src/test/java/pl/klimas7/spring/core/dependson/TestConfig.java
+++ core/src/test/java/pl/klimas7/spring/core/dependson/TestConfig.java
@@ -1,4 +1,9 @@
 package pl.klimas7.spring.core.dependson;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.klimas7.spring.core.dependson")
 class TestConfig {
 }
