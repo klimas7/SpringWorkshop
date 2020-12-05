48992c64f9a2a4a85bfaa8ae6d9a8d2e4350d119
klimas7 <klimas7@gmail.com>
2020-12-05 13:42:28 +0100

    Other example #1: Spring boot

--- core/src/main/java/pl/klimas7/spring/core/other/MainClass.java
+++ core/src/main/java/pl/klimas7/spring/core/other/MainClass.java
@@ -1,11 +1,17 @@
 package pl.klimas7.spring.core.other;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+
+@Slf4j
 public class MainClass {
     static {
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-7s] %5$s %n");
     }
 
     public static void main(String[] args) {
-
+        log.info("---------------------");
+        SpringApplication.run(AppConfiguration.class, args);
+        log.info("---------------------");
     }
 }
