793ab94d8630a639a139a41961c8fb18a35f1983
klimas7 <klimas7@gmail.com>
2020-12-05 16:22:39 +0100

    diff #3: Diff between Autowired Inject Resource: Set as name

--- core/src/test/java/pl/klimas7/spring/core/diff/DiffAutowiredTest.java
+++ core/src/test/java/pl/klimas7/spring/core/diff/DiffAutowiredTest.java
@@ -12,10 +12,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(classes = {TestConfig.class})
 public class DiffAutowiredTest {
     @Autowired
-    private Circle shape;
+//    private Circle shape;
+    private Shape square;
 
     @Test
     public void startContextTest() {
-        log.info(shape.description());
+//        log.info(shape.description());
+        log.info(square.description());
     }
 }
