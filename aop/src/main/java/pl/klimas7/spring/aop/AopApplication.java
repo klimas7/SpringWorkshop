e3862768fbda0006ee7c19648a605e40ef905768
klimas7 <klimas7@gmail.com>
2020-12-06 11:27:33 +0100

    AOP #1: some shopping

--- aop/src/main/java/pl/klimas7/spring/aop/AopApplication.java
+++ aop/src/main/java/pl/klimas7/spring/aop/AopApplication.java
@@ -3,7 +3,9 @@ package pl.klimas7.spring.aop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class AopApplication {
     public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class);
     }
 }
--- aop/src/main/java/pl/klimas7/spring/aop/IkeaShop.java
+++ aop/src/main/java/pl/klimas7/spring/aop/IkeaShop.java
@@ -1,4 +1,18 @@
 package pl.klimas7.spring.aop;
 
-public class IkeaShop {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class IkeaShop implements Shop {
+    @Override
+    public void buy() {
+        log.info("Kupuję piękną komodę");
+    }
+
+    @Override
+    public void addToBasket(String name) {
+        log.info("Add to basket: " + name);
+    }
 }
