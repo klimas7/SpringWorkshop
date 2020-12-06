08481d70bac42427455134cafabf9ddee123a3ab
klimas7 <klimas7@gmail.com>
2020-12-06 14:25:04 +0100

    jmx #6: hide operations part

--- jmx/src/main/java/pl/klimas7/spring/jmx/JmxConfiguration.java
+++ jmx/src/main/java/pl/klimas7/spring/jmx/JmxConfiguration.java
@@ -3,6 +3,8 @@ package pl.klimas7.spring.jmx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.jmx.export.assembler.InterfaceBasedMBeanInfoAssembler;
+import org.springframework.jmx.export.assembler.MethodExclusionMBeanInfoAssembler;
 import org.springframework.jmx.export.assembler.MethodNameBasedMBeanInfoAssembler;
 import org.springframework.jmx.support.ConnectorServerFactoryBean;
 import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
@@ -45,13 +47,31 @@ public class JmxConfiguration {
     }
 
     @Bean
+    public MethodExclusionMBeanInfoAssembler excludeAssembler() {
+        MethodExclusionMBeanInfoAssembler methodExclusionMBeanInfoAssembler = new MethodExclusionMBeanInfoAssembler();
+        methodExclusionMBeanInfoAssembler.setIgnoredMethods("setPrivateMessage");
+        return methodExclusionMBeanInfoAssembler;
+    }
+
+    @Bean
+    public InterfaceBasedMBeanInfoAssembler interfaceAssembler() {
+        InterfaceBasedMBeanInfoAssembler interfaceBasedMBeanInfoAssembler = new InterfaceBasedMBeanInfoAssembler();
+        interfaceBasedMBeanInfoAssembler.setManagedInterfaces(MessageManageOperation.class);
+        return interfaceBasedMBeanInfoAssembler;
+    }
+
+    @Bean
     public MBeanExporter mBeanExporter(MessageManageOperation messageManageOperation,
-                                       MethodNameBasedMBeanInfoAssembler assembler) {
+                                       MethodNameBasedMBeanInfoAssembler assembler,
+                                       MethodExclusionMBeanInfoAssembler excludeAssembler,
+                                       InterfaceBasedMBeanInfoAssembler interfaceAssembler) {
         MBeanExporter mBeanExporter = new MBeanExporter();
         Map<String, Object> beans = new HashMap<>();
         beans.put("jmxApplication:name=MessageManageOperation", messageManageOperation);
         mBeanExporter.setBeans(beans);
-        mBeanExporter.setAssembler(assembler);
+        //mBeanExporter.setAssembler(assembler);
+//        mBeanExporter.setAssembler(excludeAssembler);
+        mBeanExporter.setAssembler(interfaceAssembler);
         return mBeanExporter;
     }
 
