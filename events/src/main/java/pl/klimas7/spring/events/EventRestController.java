734d66d659944bf0c5d2bdbaf71858b201c2f881
klimas7 <klimas7@gmail.com>
2020-12-06 12:47:13 +0100

    Events #1: Basic application

--- events/src/main/java/pl/klimas7/spring/events/EventRestController.java
+++ events/src/main/java/pl/klimas7/spring/events/EventRestController.java
@@ -1,5 +1,19 @@
 package pl.klimas7.spring.events;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
 public class EventRestController {
 
+    @PostMapping("/processMessage/{message}")
+    public void processMessage(@PathVariable String message) {
+        log.info("Process message: " + message);
+    }
 }
--- events/src/main/java/pl/klimas7/spring/events/EventsApplications.java
+++ events/src/main/java/pl/klimas7/spring/events/EventsApplications.java
@@ -1,7 +1,12 @@
 package pl.klimas7.spring.events;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EventsApplications {
     public static void main(String[] args) {
+        SpringApplication.run(EventsApplications.class);
     }
 
     /*
